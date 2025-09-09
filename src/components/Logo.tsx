485a291641e9fe9f71b07934f6aef2a02ea388bf
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2025-09-10 02:45:57 +0530

    fix: navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,13 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 import Artium from "@/assets/Artium_logo.svg";
 
 export const Logo = () => {
-  return (
-    <Link href="/" className="flex items-center">
-      <Image 
-        src={Artium} 
-        alt="Artium Logo"
-        width={120}
-        height={50}
-        priority
-      />
-    </Link>
-  );
+	return (
+		<Link href="/" className="flex items-center">
+			<Image src="" alt="Inkspire" width={120} height={50} priority />
+		</Link>
+	);
 };
 
 export default Logo;
